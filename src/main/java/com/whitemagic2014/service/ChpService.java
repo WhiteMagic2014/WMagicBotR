ae0760bd79afce350cf3ed79ad04e047f799272f
magic chen <418379149@qq.com>
2022-07-18 11:50:51 +0800

    -- 夸夸指令

---
+++ src/main/java/com/whitemagic2014/service/ChpService.java
@@ -0,0 +1,13 @@
+package com.whitemagic2014.service;
+
+/**
+ * @Description: 夸夸service
+ * @author: magic chen
+ * @date: 2022/7/18 11:26
+ **/
+public interface ChpService {
+
+
+    String getChp();
+
+}
