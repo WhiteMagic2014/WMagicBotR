30d6aecfb1574a0a95d49336b05b743bd7b5009d
magic chen <418379149@qq.com>
2023-02-06 11:30:16 +0800

    -- 优化广播功能

--- src/main/java/com/whitemagic2014/command/impl/group/funny/PenCommand.java
+++ src/main/java/com/whitemagic2014/command/impl/group/funny/PenCommand.java
@@ -13,7 +13,7 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.Random;
 
-@Command
+//@Command
 public class PenCommand extends NoAuthCommand {
 
     Random random = new Random();
